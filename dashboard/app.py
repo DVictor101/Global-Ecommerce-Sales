58d12ea11e880d4a63bd5e4ed2a0bf1b6fb4d935
Segun(RenewedCrown) <seguntimileyin489@gmail.com>
2026-07-25 02:20:02 +0100

    fixing style path

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from pathlib import Path
 
 #stetting preferred padding
 st.markdown("""
@@ -35,8 +36,9 @@ st.set_page_config(
 
 
 def load_css():
+    css_file = Path(__file__).parent / "assets" / "style.css"
 
-    with open("./assets/style.css") as f:
+    with open(css_file) as f:
         st.markdown(
             f"<style>{f.read()}</style>",
             unsafe_allow_html=True
